72a885142ad5081153a6356b7f5a4dd6cf09a10c
Vitaliy Atamanjuk <atamanjuk@infis.cz>
2025-09-12 11:34:28 +0200

    Přidán filter zemí podle kontinentu, maximální a minimální populace a průměrný věk dožití všech zemí světa

--- src/basics/ParseFile.java
+++ src/basics/ParseFile.java
@@ -2,27 +2,85 @@ package basics;
 
 import fileworks.DataImport;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class ParseFile {
 
     public static void main(String[] args) {
-     //zkus nacist soubor
+        //zkus nacist soubor
         DataImport di = new DataImport("countries.txt");
-        di.printFile();
-        System.out.println("Prvni zeme:");
-        String countryData = di.readLine();
+        // di.printFile();
+//        System.out.println("Prvni zeme:");
+/*        String countryData = di.readLine();
         //rozdel na kusy
-        String[] dataPieces = countryData.split(";");
-        Country firstCountry = new Country(
+       // ctrl + /
+//        Country firstCountry = parseLine(countryData);
+//
+//        System.out.println(firstCountry.getContinent());
+//        System.out.println(firstCountry.getAge());
+//        System.out.println(firstCountry);*/
+        // hasNext() - vrátí true/false
+
+        ArrayList<Country> countries = new ArrayList<>();
+        while (di.hasNext()) {
+            Country country = parseLine(di.readLine());
+            countries.add(country);
+        }
+        filterByContinent(countries, "Europe");
+        maxAndMin(countries);
+        avg(countries);
+        di.finishImport();
+    }
+
+    static Country parseLine(String line) {
+        String[] dataPieces = line.split(";");
+
+        Country country = new Country(
                 dataPieces[0],
                 dataPieces[1],
                 Long.parseLong(dataPieces[2]),
                 Double.parseDouble(dataPieces[3])
         );
-        System.out.println(firstCountry.getContinent());
-        System.out.println(firstCountry.getAge());
-        di.finishImport();
+
+        return country;
+    }
+
+    static void filterByContinent(ArrayList<Country> c, String filter) {
+        // Filter = North America
+        int totalCount = 0;
+        for (Country country : c) {
+            if (country.getContinent().equalsIgnoreCase(filter)) {
+                System.out.println(country);
+                totalCount++;
+            }
+        }
+        System.out.printf("Celkový počet zemí v %s je %d\n", filter, totalCount);
+    }
+
+    static void avg(ArrayList<Country> c){
+        double total = 0;
+        for(Country cr : c){
+            total += cr.getAge();
+        }
+        System.out.println(total / c.size());
+    }
+
+    static void maxAndMin(ArrayList<Country> c) {
+        int minIndex = 0, maxIndex = 0;
+        for (int i = 0; i < c.size(); i++) {
+            if (c.get(minIndex).getSomething() > c.get(i).getSomething()) {
+                minIndex = i;
+            }
+            if (c.get(maxIndex).getSomething() < c.get(i).getSomething()) {
+                maxIndex = i;
+            }
+        }
+        System.out.println(c.get(minIndex));
+        System.out.println(c.get(maxIndex));
     }
 }
+
 class Country {
     public String name, continent;
     public long something;
@@ -34,6 +92,7 @@ class Country {
         this.something = something;
         this.age = age;
     }
+
 
     public String getName() {
         return name;
@@ -65,6 +124,11 @@ class Country {
 
     public void setAge(double age) {
         this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Název: %s | Kontinent: %s | Populace: %,d | Dožití: %.2f", name, continent, something, age);
     }
 }
 
