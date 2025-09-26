623d254c657d5d0b8d6201b092db96043250ffea
Vitaliy Atamanjuk <atamanjuk@infis.cz>
2025-09-26 11:34:57 +0200

    Added solution to hunter party

--- src/oop/inheritance/HuntingSeason.java
+++ src/oop/inheritance/HuntingSeason.java
@@ -1,26 +1,49 @@
 package oop.inheritance;
 
+import fileworks.DataImport;
+
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Random;
 
 public class HuntingSeason {
 
-    static void doRandomJob(Hunter hunter){
+    static void doRandomJob(Hunter hunter) {
         Random r = new Random();
 
-        switch(r.nextInt(1,4)){
+        switch (r.nextInt(1, 4)) {
             case 1:
                 hunter.hunt();
                 break;
             case 2:
-                ((Soldier) hunter).killBandits();
+                if (hunter instanceof Soldier) {
+                    ((Soldier) hunter).killBandits();
+                }
                 break;
             case 3:
-                ((Witcher) hunter).slayMonster();
+                if (hunter instanceof Witcher) {
+                    ((Witcher) hunter).slayMonster();
+                }
                 break;
         }
     }
 
+    static void doJob(Hunter hunter, String pismeno) {
+        // když role je "M"
+        if (pismeno.equals("M") && hunter instanceof Witcher) {
+            ((Witcher) hunter).slayMonster();
+            return;
+        }
+
+        // když role je "K"
+        if (pismeno.equals("K") && hunter instanceof Soldier) {
+            ((Soldier) hunter).killBandits();
+            return;
+        }
+
+        // když role je "H"
+        hunter.hunt();
+    }
 
 
     public static void main(String[] args) {
@@ -50,14 +73,41 @@ public class HuntingSeason {
 //    ((Witcher) another).slayMonster();
 
         ArrayList<Hunter> hunters = new ArrayList<>();
-        hunters.add(new Hunter("Hornet"));
+        hunters.add(new Soldier("Hornet"));
         hunters.add(new Soldier("George S Patton"));
         hunters.add(new Soldier("Robert Duvall"));
-        hunters.add(new Witcher("Geralt"));
-        hunters.add(new Witcher("Coën"));
+        hunters.add(new Soldier("Geralt"));
+        hunters.add(new Soldier("Coën"));
 
-        for(Hunter hunter : hunters){
-            doRandomJob(hunter);
+        // Načíst data
+        DataImport di = new DataImport("hunters.txt");
+
+        Hunter hunter1;
+
+        while (di.hasNext()) {
+            String line = di.readLine();
+            String[] parts = line.split(",");
+
+            if(parts[1].equals("Hunter")){
+                hunter1 = new Hunter(parts[0]);
+            }
+
+            else if(parts[1].equals("Soldier")){
+                hunter1 = new Soldier(parts[0]);
+            }
+            // zaklínač
+            else {
+                hunter1 = new Witcher(parts[0]);
+            }
+
+            for (int i = 2; i < parts.length; i++) {
+                doJob(hunter1, parts[i]);
+            }
+
+            System.out.println(hunter1.name + " si vydělal " + hunter1.gold);
         }
+
+        di.finishImport();
+
     }
 }
