beabf91b8e5a047f33918e1d25f51b681fa138d9
Vitalij Atamanjuk <atamanjuk@infis.cz>
2025-11-07 09:56:45 +0100

    ADT - intro

---
+++ src/adt/ArrayUtils.java
@@ -0,0 +1,75 @@
+package adt;
+
+/**
+ * Pomocná třída pro práci s celočíselným polem.
+ */
+public class ArrayUtils {
+
+    /**
+     * Metoda vypíše všechny prvky v poli <b>arr</b>.
+     * Jednotlivé prvky mohou být vypsány na jeden řádek oddělené mezerou
+     * nebo na více řádků, podle implementace.
+     *
+     * @param arr celočíselné pole, jehož obsah má být vypsán
+     */
+    public void printArray(int[] arr) {
+        // TODO: implementovat
+    }
+    /**
+     * Metoda porovná dvě celočíselná pole a určí, zda jsou shodná.
+     * Pole jsou shodná, pokud mají stejnou délku a stejné hodnoty
+     * na stejných indexech.
+     *
+     * @param arr1 první pole
+     * @param arr2 druhé pole
+     * @return true pokud jsou obě pole shodná, jinak false
+     */
+    public boolean equals(int[] arr1, int[] arr2) {
+        // TODO: implementovat
+        return false;
+    }
+
+    /**
+     * Metoda vrátí největší prvek v poli.
+     *
+     * @param arr vstupní celočíselné pole
+     * @return největší nalezená hodnota v poli
+     */
+    public int getMax(int[] arr) {
+        // TODO: implementovat
+        return 0;
+    }
+
+    /**
+     * Metoda vrátí nejmenší prvek v poli.
+     *
+     * @param arr vstupní celočíselné pole
+     * @return nejmenší nalezená hodnota v poli
+     */
+    public int getMin(int[] arr) {
+        // TODO: implementovat
+        return 0;
+    }
+
+    /**
+     * Metoda vrátí aritmetický průměr všech prvků v poli.
+     *
+     * @param arr vstupní celočíselné pole
+     * @return průměrná hodnota prvků pole jako desetinné číslo
+     */
+    public double getAverage(int[] arr) {
+        // TODO: implementovat
+        return 0;
+    }
+
+    /**
+     * Metoda vrátí součet všech prvků v poli.
+     *
+     * @param arr vstupní celočíselné pole
+     * @return celkový součet všech prvků v poli
+     */
+    public int sum(int[] arr) {
+        // TODO: implementovat
+        return 0;
+    }
+}
